f91c324fa209652c3958c94520985debd3b4dea4
Davro2005 <116736664+Davro2005@users.noreply.github.com>
2022-11-10 23:38:22 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,64 @@
+//task2
+//let a=prompt("inter1");
+//let b=prompt("inter2");
+//console.log(+b%+a);
+//ta+sk3
+//let a=prompt("int");
+//console.log(+a%10);
+//task4
+//let a=prompt("int");
+//console.log(Math.floor(+a/10));
+//task5
+//let a=prompt("int");
+//console.log(Math.floor(+a/10%10));
+//task6
+//let a=prompt("int");
+//console.log(Math.floor((+a/10%10)+Math.floor(+a/100)+Math.floor(+a%10)));
+//task7
+//let a=prompt("int");
+//console.log(+a*4);
+//task8
+//let a=prompt("int");
+//let b=prompt("int2");
+//let p=4*+a;
+//let s=2*+a+2*+b
+//console.log(p);
+//console.log(s);
+//task9
+//let a=prompt("int");
+//let b=prompt("int2");
+//let c=+a+ +b;
+//console.log(+c/2)
+//task10
+//let a=+prompt("int");
+//let b=+prompt("int2");
+//let c=Math.sqrt(a*a+b*b);
+//console.log(c);
+//task10a
+//let a=+prompt("int");
+//let b=+prompt("int2");
+//a>b?console.log(a):console.log(b)
+//task10a
+//let a=+prompt("int");
+//let b=+prompt("int2");
+//a>b?console.log(b+" "+a):console.log(a+" "+b);
+//task10a
+//let a=+prompt("int");
+//a>0?console.log(a+1):console.log(0);
+//task10a
+//let a=+prompt("int");
+//a>0?console.log(a+1):console.log(a-2);
+//task5
+let cnt=0;
+for(let i=1;i<=3;i++)
+{
+    let b=+prompt("Enter b");
+    if(b>0)
+    {
+        cnt++;
+    }
+}
+console.log(cnt)
+
+
+
